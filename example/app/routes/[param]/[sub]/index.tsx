4fa727d58bfefe7f8524cd9e20db189e6473a5c6
swwind <i@sww.moe>
2024-02-21 15:40:30 +0800

    improve example

--- example/app/routes/[param]/[sub]/index.tsx
+++ example/app/routes/[param]/[sub]/index.tsx
@@ -1,11 +1,12 @@
+import { useParam } from "@biliblitz/blitz";
 import { meta$ } from "@biliblitz/blitz/server";
 
 export const meta = meta$((evt) => {
   const param = evt.params.get("sub")!;
-
   return { title: `Sub - ${param}` };
 });
 
 export default () => {
-  return <div>sub = {2333}</div>;
+  const sub = useParam("sub");
+  return <div>sub = {sub}</div>;
 };
--- example/app/routes/[param]/index.tsx
+++ example/app/routes/[param]/index.tsx
@@ -1,11 +1,12 @@
+import { useParam } from "@biliblitz/blitz";
 import { meta$ } from "@biliblitz/blitz/server";
 
 export const meta = meta$((evt) => {
   const param = evt.params.get("param")!;
-
   return { title: `Param - ${param}` };
 });
 
 export default () => {
-  return <div>param = {2333}</div>;
+  const param = useParam("param");
+  return <div>param = {param}</div>;
 };
