1b91e1681f1327b5157be0d2e4e891236133c3b5
swwind <i@sww.moe>
2026-03-10 16:55:10 +0800

    Update GitHub link in VitePress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "首页", link: "/" },
-      { text: "GitHub", link: "https://github.com/biliblitz/blitz" },
     ],
 
     sidebar: [
@@ -44,7 +43,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      { icon: "github", link: "https://github.com/biliblitz/blitz" },
     ],
   },
 });
